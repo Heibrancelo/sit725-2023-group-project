7dc8dc917e3743436286e98cc2944db2ebc63ec2
sandriyaf <sandriyafernandes10@gmail.com>
2023-10-04 12:53:32 +1100

    Edit,remove items from cart and bill summary

---
+++ orders/cart.js
@@ -0,0 +1,70 @@
+let discountApplied = false;
+
+// JavaScript functions to update quantity
+function updateQuantity(item, action) {
+    const quantityElement = document.getElementById(item + "-quantity");
+    let quantity = parseInt(quantityElement.textContent);
+
+    if (action === "increment") {
+        quantity += 1;
+    } else if (action === "decrement" && quantity > 1) {
+        quantity -= 1;
+    }
+
+    quantityElement.textContent = quantity;
+
+    // Calculate the total cart price and total items
+    calculateTotal();
+}
+
+// Function to calculate the total cart price and total items
+function calculateTotal() {
+    const quantities = document.querySelectorAll(".border");
+    const prices = document.querySelectorAll(".price");
+
+    let total = 0;
+    let totalItems = 0;
+
+    quantities.forEach(function (quantityElement, index) {
+        const quantity = parseInt(quantityElement.textContent);
+        const priceText = prices[index].textContent.trim();
+        const price = parseFloat(priceText.replace('$', ''));
+        total += quantity * price;
+        totalItems += quantity;
+    });
+
+    // If a discount is applied, reduce the total by 10%
+    
+    // Update the total price and total items in the summary
+    const subTotalPriceDisplay = document.getElementById("total-price-display");
+    subTotalPriceDisplay.textContent = "SUB TOTAL $ " + total.toFixed(2);
+
+    const totalPriceDisplay = document.getElementById("display-total-price");
+    total += 5;
+    if (discountApplied) {
+        total *= 0.9; // 10% discount
+    }
+    totalPriceDisplay.textContent = "$ " + total.toFixed(2);
+
+    const totalItemsDisplay = document.getElementById("total-items-display");
+    totalItemsDisplay.textContent = "ITEMS " + totalItems;
+}
+
+// Function to apply the discount
+function applyDiscount() {
+    const codeInput = document.getElementById("code").value;
+    if (codeInput === "DISCOUNT10") {
+        discountApplied = true;
+        calculateTotal(); // Recalculate the total with the discount
+    } else {
+        discountApplied = false;
+    }
+}
+
+function removeProduct(closeButton) {
+    const productElement = closeButton.closest('.row');
+    productElement.remove();
+
+    // Calculate the total cart price and total items after removing the product
+    calculateTotal();
+}
