977dcc5b2cae231b9e53cca1bb13907b9f34be4e
Syed-Sarfaraz-Syed <s222599005@deakin.edu.au>
2023-10-10 04:22:03 +1100

    Heibrance code

---
+++ addstalls.js
@@ -0,0 +1,71 @@
+
+const addStallCards = (stalls) => {
+    stalls.forEach(stall => {
+        let stallCard = `<div class="col s12 m6 l3"><div class="card" id="stall-${stall._id}">`+
+        `<div class="card-image waves-effect waves-block waves-light">`+ 
+        `<img class="activator" src="${stall.imagePath}"></div>`+
+        `<div class="card-content"><span class="card-title">${stall.stallName}</span>`+
+        `<p>${stall.description}</p></div><div class="card-action center"><a href="#">Enter</a></div></div></div>`
+
+
+        $("#stall-section").append(stallCard);
+    });
+}
+
+
+const stallformSubmitted = () => {
+    console.log("Form submit initiated.");
+
+    let stallformData = {};
+    stallformData.stallName = $('#stallName').val();
+    stallformData.description = $('#stallDescription').val();
+    stallformData.imagePath = $('#imagePath').val();  
+
+    console.log("Form data prepared:", stallformData);
+    postStall(stallformData);
+}
+
+
+
+function postStall(stall) {
+    $.ajax({
+        url: '/api/stallInfo',
+        type: 'POST',
+        data: JSON.stringify(stall),
+        contentType: 'application/json',
+        success: (result) => {
+            console.log("Server response received:", result);
+            if (result.statusCode === 201) {
+                alert('Stall added successfully.');
+                getAllStalls();
+            } else {
+                console.error("Unexpected server response:", result);
+            }
+        },
+        error: (xhr, status, error) => {
+            console.error("Error in postStall AJAX:", error);
+        }
+    });
+}
+
+
+
+
+
+
+function getAllStalls() {
+    $.get('/api/stallInfo', (response) => {
+        if (response.statusCode === 200) {
+            addStallCards(response.data);
+        }
+    });
+}
+
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+    $('#stall-formSubmit').click(() => {
+        stallformSubmitted();
+    });
+    $('.modal').modal();
+    getAllStalls();
+});
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 let express = require('express');
 let app = express();
+const path = require('path');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://akashdeepakashdeep271291:Ar1un%408ha@cluster0.iopmovp.mongodb.net/?retryWrites=true&w=majority";
 let port = process.env.port || 3000;
 let collection;
 let collection2;
 let collection3;
+let collectionStall;
+let menuCollection;
 
 app.use(express.static(__dirname + '/'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/images', express.static(__dirname + '/images'));
 
 
 const client = new MongoClient(uri, {
@@ -20,11 +24,12 @@ const client = new MongoClient(uri, {
     }
 });
 
+
 async function runDBConnection() {
     try {
         await client.connect();
-        collection = client.db().collection('Signup');
-        console.log(collection);
+       collection = client.db().collection('Signup');
+       console.log(collection);
     } catch (ex) {
         console.error(ex);
     }
@@ -33,8 +38,8 @@ async function runDBConnection() {
 async function runDBConnection2() {
     try {
         await client.connect();
-        collection2 = client.db().collection('Feedback');
-        console.log(collection2);
+       collection2 = client.db().collection('Feedback');
+       console.log(collection2);
     } catch (ex) {
         console.error(ex);
     }
@@ -43,11 +48,33 @@ async function runDBConnection2() {
 async function runDBConnection3() {
     try {
         await client.connect();
-        collection3 = client.db().collection('Login');
-        console.log(collection3);
+       collection3 = client.db().collection('Login');
+       console.log(collection3);
     } catch (ex) {
         console.error(ex);
     }
+}
+
+async function runDBConnectionStall() {
+    try {
+        await client.connect();
+       collectionStall = client.db().collection('ListofStalls');
+       console.log(collectionStall);
+    } catch (ex) {
+        console.error(ex);
+    }
+}
+
+async function runDBmenuCollection() {
+    try {
+        await client.connect();
+        menuCollection = client.db().collection("Menus");
+        console.log("Connected to MongoDB");
+        console.log(menuCollection);
+    }catch (ex) {
+        console.error(ex);
+    } 
+
 }
 
 app.get('/', function (req, res) {
@@ -55,7 +82,38 @@ app.get('/', function (req, res) {
 });
 
 
-app.post('/api/signup', (req, res) => {
+app.get('/menu', async function (req, res) {
+    try {
+        const restaurantId = req.query.id;
+        const name = req.query.name || '';
+        const menuItems = await menuCollection.find({ restaurentId: restaurantId }).toArray();
+        res.render('menu', { menuItems, name, restaurantId });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+
+
+//mongodb id
+app.get('/filterItems/:itemId', async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+        const filteredItems = await menuCollection.find({ _id: itemId }).toArray();
+        res.json(filteredItems);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.post('/api/signup', (req,res)=>{
     let signup = req.body;
     postSignup(signup);
 });
@@ -64,7 +122,7 @@ function postSignup(signup) {
     collection.insertOne(signup);
 }
 
-app.post('/api/feedback', (req, res) => {
+app.post('/api/feedback', (req,res)=>{
     let feedback = req.body;
     postFeedback(feedback);
 });
@@ -73,7 +131,7 @@ function postFeedback(feedback) {
     collection2.insertOne(feedback);
 }
 
-app.post('/api/login', (req, res) => {
+app.post('/api/login', (req,res)=>{
     let login = req.body;
     postLogin(login);
 });
@@ -87,4 +145,57 @@ app.listen(port, () => {
     runDBConnection();
     runDBConnection2();
     runDBConnection3();
+    runDBConnectionpayment();
+    runDBmenuCollection();
 });
+
+async function runDBConnectionpayment() {
+    try {
+        await client.connect();
+       collectionpayment = client.db().collection('PaymentDetails');
+       console.log(collectionpayment);
+    } catch (ex) {
+        console.error(ex);
+    }
+}
+function postDetails(payment) {
+    collectionpayment.insertOne(payment);
+}
+app.post('/api/payment', (req,res)=>{
+    let payment = req.body;
+    postDetails(payment);
+});
+
+
+
+// Add Stall Collection
+app.get('/api/stallInfo', (req,res) => {
+    getStallInfo((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200, data:result, message:'Showing all stalls now'});
+        }
+    });
+});
+
+
+app.post("/api/stallInfo", (req, res) => {
+    let stallInfo = req.body;
+    postStallInfo(stallInfo, res);
+});
+
+
+function postStallInfo(stallInfo, res) {
+    collectionStall.insertOne(stallInfo, (err, result) => {
+        if (err) {
+            console.error("Error inserting stall info:", err);
+            res.status(500).json({ error: "Internal Server Error" });
+        } else {
+            res.status(201).json({ message: "Stall added successfully" });
+        }
+    });
+ } 
+
+
+function getStallInfo(callback){
+    collectionStall.find({}).toArray(callback);
+}
